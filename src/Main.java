0670d527c86fc967e5b5890a9f52fdfd5b4bb12d
Khal01 <aliyev_56@list.ru>
2023-10-04 01:36:21 +0300

    first6task

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+public class Main
+{ public static void main(String[] args) {
+
+    System.out.println("Задача1");
+        var dog = 8.0;
+                System.out.println(dog);
+         var cat = 3.6;
+                System.out.println(cat);
+        var peper = 763789;
+                System.out.println(peper);
+    System.out.println("Задача2");
+                dog=dog + 4;
+    System.out.println(dog);
+                cat=cat + 4;
+    System.out.println(cat);
+    peper=peper+4;
+    System.out.println(peper);
+    System.out.println("Задача3");
+    dog=dog - 3.5;
+    System.out.println(dog);
+    cat=cat - 3.5;
+    System.out.println(cat);
+    peper=peper-4;
+    System.out.println(peper);
+    System.out.println("Задача4");
+    var friend = 19;
+    System.out.println(friend);
+    friend=friend + 2;
+    System.out.println(friend);
+    friend=friend/7;
+    System.out.println(friend);
+    System.out.println("Задача5");
+    var frog=3.5;
+    System.out.println(frog);
+    frog=frog *10;
+    System.out.println(frog);
+    frog=frog /3.5;
+    System.out.println(frog);
+    frog=frog +4;
+    System.out.println(frog);
+    System.out.println("Задача6");
+    var weightBoxer1=78.2;
+    System.out.println(weightBoxer1);
+    var weightBoxer2= 82.7;
+    System.out.println(weightBoxer2);
+    var totallyWeightBoxer= weightBoxer1 + weightBoxer2;
+    System.out.println(totallyWeightBoxer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
